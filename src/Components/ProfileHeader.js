64d200d5c69e44f999ed4c1d4c8622c681171499
Abid Ali KP <apathikkal@mvement,co>
2023-04-10 00:18:32 +0530

    icon changes

--- src/Components/ProfileHeader.js
+++ src/Components/ProfileHeader.js
@@ -1,10 +1,11 @@
+import ProPic from "../assets/images/logo512.png";
 const Header = () => {
   const dim = 150;
   return (
     <div className="flex flex-col items-center justify-center w-full gap-2 px-24 py-12 border rounded-lg bg-stone-700 hover:border-stone-500 border-stone-700">
       <img
         className="object-cover rounded-full"
-        src="https://source.unsplash.com/featured/1"
+        src={ProPic}
         alt=""
         style={{ maxHeight: dim, maxWidth: dim, minHeight: dim, minWidth: dim }}
       />
